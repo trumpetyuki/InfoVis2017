c271ace1f087fe4b9c66c88b57dc7f31007cb8b6
野田　祐希 <173x218x@istc-c-005.epc.center.kobe-u.ac.jp>
2017-05-11 15:21:20 +0900

    Initial commit

--- W08/main02.js
+++ W08/main02.js
@@ -1,7 +1,7 @@
 function main()
 {
-    var width = 500;
-    var height = 500;
+    var width = 1000;
+    var height = 1000;
 
     var scene = new THREE.Scene();
 
@@ -27,12 +27,26 @@ function main()
         vertexShader: document.getElementById('shader.vert').text,
         fragmentShader: document.getElementById('shader.frag').text,
 	uniforms: {
-	    light_position: { type: 'v3', value: light.position } }
+	    light_position: { type: 'v3', value: light.position }}
+    });
+
+    var geometry2 = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material2 = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('gouraud.vert').text,
+        fragmentShader: document.getElementById('gouraud.frag').text,
+	uniforms: {
+	    light_position: { type: 'v3', value: light.position }}
     });
 
     var torus_knot = new THREE.Mesh( geometry, material );
+    torus_knot.position.x=-1;
+    torus_knot.scale.set(0.5,0.5,0.5);
+    var torus_knot2 = new THREE.Mesh( geometry2, material2 );
+    torus_knot2.scale.set(0.5,0.5,0.5);
+    torus_knot2.position.x=1;
     scene.add( torus_knot );
-
+    scene.add( torus_knot2 );
     loop();
 
     function loop()
@@ -40,6 +54,8 @@ function main()
         requestAnimationFrame( loop );
         torus_knot.rotation.x += 0.01;
         torus_knot.rotation.y += 0.01;
+	torus_knot2.rotation.x += 0.01;
+        torus_knot2.rotation.y += 0.01;
         renderer.render( scene, camera );
     }
 }
